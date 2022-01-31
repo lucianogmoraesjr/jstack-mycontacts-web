d94909aa15c2569406f00dfb5db579e93c4a9042
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-01-31 18:49:14 -0300

    feat: create ContactsList card

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+import {
+  Card, Container, Header, ListContainer,
+} from './styles';
 
 export function ContactsList() {
   return (
@@ -7,6 +13,78 @@ export function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Ordenar" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Luciano Moraes</strong>
+              <small>instagram</small>
+            </div>
+            <span>luciano@mail.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Editar" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Excluir" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Luciano Moraes</strong>
+              <small>instagram</small>
+            </div>
+            <span>luciano@mail.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Editar" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Excluir" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Luciano Moraes</strong>
+              <small>instagram</small>
+            </div>
+            <span>luciano@mail.com.br</span>
+            <span>(51) 99999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Editar" />
+            </a>
+
+            <button type="button">
+              <img src={trash} alt="Excluir" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
