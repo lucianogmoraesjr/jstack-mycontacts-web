22db2a187c156da2d9f9f1803c29622fa18b41c6
Luciano Moraes Jr <lucianogmoraesjr@gmail.com>
2022-02-05 18:27:51 -0300

    feat: modal partial

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
+  padding: 0 16px;
   border: none;
   border-radius: 4px;
   background: ${({ theme }) => theme.colors.primary.main};
@@ -26,4 +26,16 @@ export default styled.button`
     background: #ccc;
     cursor: not-allowed;
   }
+
+  ${({ theme, danger }) => danger && css`
+    background: ${theme.colors.danger.main};
+
+    &:hover {
+    background: ${theme.colors.danger.light};
+    }
+
+    &:active {
+      background: ${theme.colors.danger.dark};
+    }
+  `}
 `;
---
+++ src/components/Modal/index.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+import Button from '../Button';
+
+import { Container, Footer, Overlay } from './styles';
+
+export function Modal({ danger }) {
+  return (
+    <Overlay>
+      <Container danger={danger}>
+        <h1>Title</h1>
+
+        <p>Body</p>
+
+        <Footer>
+          <button type="button" className="cancel-button">Cancelar</button>
+          <Button type="button" danger={danger}>Deletar</Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
